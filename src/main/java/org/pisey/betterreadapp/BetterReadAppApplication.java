40ba633333945df42f5be7b97271e351abd6dce3
HOUR Chan Pisey <77218571+Chanpisey-github@users.noreply.github.com>
2022-02-20 21:04:03 +0700

    oauth-to-github

--- src/main/java/org/pisey/betterreadapp/BetterReadAppApplication.java
+++ src/main/java/org/pisey/betterreadapp/BetterReadAppApplication.java
@@ -2,12 +2,23 @@ package org.pisey.betterreadapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BetterReadAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterReadAppApplication.class, args);
+	}
+
+	@RequestMapping("/users")
+	public String user(@AuthenticationPrincipal OAuth2User principal){
+		System.out.println(principal);
+		return principal.getAttribute("name");
 	}
 
 }
