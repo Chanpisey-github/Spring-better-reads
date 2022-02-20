e3d486271dbec6f0480a5367ff72a7c670ae0b50
HOUR Chan Pisey <77218571+Chanpisey-github@users.noreply.github.com>
2022-02-20 17:57:26 +0700

    freshInstall Dependencies

---
+++ src/main/java/org/pisey/betterreadapp/BetterReadAppApplication.java
@@ -0,0 +1,13 @@
+package org.pisey.betterreadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReadAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pisey/betterreadapp/BetterReadAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pisey.betterreadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
