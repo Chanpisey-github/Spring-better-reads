5995e5e6f7c8bccb586157cca7fac2be21d7c254
HOUR Chan Pisey <77218571+Chanpisey-github@users.noreply.github.com>
2022-03-03 01:08:21 +0700

    Revert "oauth-to-github"
    This reverts commit 40ba633333945df42f5be7b97271e351abd6dce3.

--- src/main/java/org/pisey/betterreadapp/BetterReadAppApplication.java
+++ src/main/java/org/pisey/betterreadapp/BetterReadAppApplication.java
@@ -3,6 +3,7 @@ package org.pisey.betterreadapp;
 import org.pisey.betterreadapp.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,16 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 @EnableCassandraRepositories
+=======
+
+@SpringBootApplication
+>>>>>>> parent of 40ba633 (oauth-to-github)
 public class BetterReadAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterReadAppApplication.class, args);
-	}
-
-	@RequestMapping("/users")
-	public String user(@AuthenticationPrincipal OAuth2User principal){
-		System.out.println(principal);
-		return principal.getAttribute("name");
 	}
 
 }
